5eef89d86227d642e51e65599d12ea24f357c189
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-09-28 01:11:35 -0300

    Modal Implementation

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,12 +1,26 @@
 var botaoSobre = document.getElementById('botaoSobre');
 var botaoSobre2 = document.getElementById('botaoSobre2');
+var botaoRegistro = document.getElementById('registro');
+var botaoLogin = document.getElementById('login');
 
 botaoSobre.addEventListener('click', function() {
 	var container = document.getElementById('right-content-id');
 	classie.toggle(container, 'hide-right-content');
 });
 
-botaoSobre2.addEventListener('click', function() {
-	var container = document.getElementById('right-content-id');
-	classie.toggle(container, 'hide-right-content');
-});
+botaoRegistro.addEventListener('click', function(){
+    var modalRegister = document.getElementById('modalRegister');
+    if ( modalRegister.style.visibility == "visible" ) {
+        modalRegister.style.visibility = "hidden";
+    } else {
+        modalRegister.style.visibility = "visible";
+    }
+})
+botaoLogin.addEventListener('click', function(){
+    var modalLogin = document.getElementById('modalLogin');
+    if ( modalLogin.style.visibility == "visible" ) {
+        modalLogin.style.visibility = "hidden";
+    } else {
+        modalLogin.style.visibility = "visible";
+    }
+})
--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,15 @@
 module IdeasHelper
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
